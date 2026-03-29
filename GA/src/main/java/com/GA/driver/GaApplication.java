7c9a72dcf09c862c1108325ca66d571ea7f03373
Amrit Chandan Mishra <amritchandan.26@gmail.com>
2026-03-29 21:19:28 +0530

    begin

---
+++ GA/src/main/java/com/GA/driver/GaApplication.java
@@ -0,0 +1,13 @@
+package com.GA.driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaApplication.class, args);
+	}
+
+}
---
+++ GA/src/test/java/com/GA/driver/GaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GA.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
